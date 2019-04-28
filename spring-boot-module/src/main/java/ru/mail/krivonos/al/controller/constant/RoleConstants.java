3e3162582e5cf0d68f1008da3aabfa152f0548da
krivonos-alex <krivonos-al@mail.ru>
2019-04-29 01:15:53 +0300

    Added constants for roles and urls.

---
+++ spring-boot-module/src/main/java/ru/mail/krivonos/al/controller/constant/RoleConstants.java
@@ -0,0 +1,15 @@
+package ru.mail.krivonos.al.controller.constant;
+
+public class RoleConstants {
+
+    public static final String ADMIN_ROLE_NAME = "ADMINISTRATOR";
+
+    public static final String CUSTOMER_ROLE_NAME = "CUSTOMER";
+
+    public static final String WITH_PREFIX_ADMIN_ROLE_NAME = "ROLE_ADMINISTRATOR";
+
+    public static final String WITH_PREFIX_CUSTOMER_ROLE_NAME = "ROLE_CUSTOMER";
+
+    private RoleConstants() {
+    }
+}
